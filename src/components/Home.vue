6288b010ce841872891e612b723ab0b324cececf
aaron <aaron@blinkmobile.com.au>
2017-09-28 18:43:18 +1000

    renamed hello component to home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,10 +11,12 @@
   <div id="headerMain">
   <div id="headerWrapper">
       <img id="headerImg" src="../assets/BlinkMobile_Logo.png"></img><h1 id="headerTitle" class="md-title" style="flex: 1">{{ msg }}</h1> 
-      </div>    
-</div> 
-          <div v-for="service in services" class="serviceCard">
-              <md-card>
+      
+      </div>
+      <h3>Build. Deploy. Manage.</h3>
+      </div>
+          <div v-for="service in services" class="serviceCards">
+              <md-card id="serviceCard">
                 <md-card-header>
                   <md-card-header-text>
                     <div class="md-title">{{service.name}}</div>
@@ -25,6 +27,14 @@
                     <img :src="service.imgSrc">
                   </md-card-media>
                 </md-card-header>
+
+                <md-card-content>
+                <ul v-for="link in service.links">
+                  <li >
+                    {{link.url}}
+                  </li>
+                </ul>
+                </md-card-content>
 
                 <md-card-actions>
                   <md-button><img src="static/github.png" id="btnSmallGit"></md-button>
@@ -37,40 +47,73 @@
 
 <script>
 export default {
-  name: 'hello',
+  name: 'home',
   data() {
     return {
       msg: 'Developers',
       services: {
         "cdn": {
           name: "Client CLI",
-          msg: "cdn hosting",
-          imgSrc: "static/clientcli_conf.png"
+          msg: "Deploy and host your static, client-side content across multiple environments.",
+          imgSrc: "static/clientcli_conf.png",
+          links: {
+            "link1": {
+              url: "url1link"
+            }
+          }
         },
         "api": {
           name: "Server CLI",
-          msg: "api hosting",
-          imgSrc: "static/server-cli-conf.png"
+          msg: "Manage and deploy your server-side APIs using Node, Serverless Technology and AWS Lambdas.",
+          imgSrc: "static/server-cli-conf.png",
+          links: {
+            "link1": {
+              url: "url1link"
+            }
+          }
         },
         "buildbot": {
           name: "Hybrid App BuildBot",
-          msg: "Hybrid apps",
-          imgSrc: "static/buildbot-cli-conf.png"
+          msg: "Build a hybrid app using any framework, and deploy cross-platform.",
+          imgSrc: "static/buildbot-cli-conf.png",
+          links: {
+            "link1": {
+              url: "url1link"
+            }
+          }
         },
         "forms": {
           name: "Forms CLI",
-          msg: "Create Forms Components",
-          imgSrc: "static/forms_conf.png"
+          msg: "Powerful next-gen forms generator for creating front-end forms components.",
+          imgSrc: "static/forms_conf.png",
+          links: {
+            "link1": {
+              url: "url1link"
+            }
+          }
         },
         "analytics": {
           name: "Analytics SDK",
-          msg: "JavaScript & PHP Analytics SDK",
-          imgSrc: "static/analytics_conf.png"
+          msg: "Javascript library for adding custom Server-Side analytics to your API project.",
+          imgSrc: "static/analytics_conf.png",
+          links: {
+            "link1": {
+              url: "url1link"
+            }
+          }
         },
         "console": {
           name: "Analytics Console",
-          msg: "Console for creating and viewing Analytics dashboards",
-          imgSrc: "static/console_conf.png"
+          msg: "Management Console for displaying analytics dashboards with your custom data.",
+          imgSrc: "static/console_conf.png",
+          links: {
+            "link1": {
+              url: "url1link"
+            },
+            "link2": {
+              url: "url2link"
+            }
+          }
         }
       } 
     }
@@ -111,39 +154,55 @@ a {
   width: 50px;
 }
 
-.serviceCard{
+.serviceCards{
   margin: 15px;
   width: 30%;
   display:inline-block;
 }
 
+#serviceCard{
+  min-height: 300px;
+
+}
+
 #headerMain{
   width: 100%;
   align: center;
-    margin-bottom: 100px;
+  margin-bottom: 150px;
 
 }
 
 #headerImg{
   float: left;
   width: 200px;
+  display: table-cell; 
+  vertical-align: bottom;
 }
 
 #headerWrapper{
   width: 315px;
   margin: 0 auto;
   margin-top:100px;
-  position: relative;
+  height: 50px;
+  margin-bottom: 75px;
 }
 
 #headerTitle{
   float: right;
+  display: table-cell; 
+  vertical-align: bottom;
+  height: 10px;
+
 
 }
 
 #menuList{
   float: right;
   margin-right: 20px;
+}
+
+#linksList{
+    
 }
 
 </style>
